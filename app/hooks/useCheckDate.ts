6c995606ed8cdc0449230d4d609e0427d5a7fea8
robin-sevelin <robinsevelin@hotmail.com>
2024-01-22 10:46:22 +0100

    async in check date

--- app/hooks/useCheckDate.ts
+++ app/hooks/useCheckDate.ts
@@ -4,31 +4,27 @@ import { createTransactionDocument } from '@/firebase/operations/createTransacti
 import { CURRENT_DATE } from '../constants/constants';
 import { ITransaction } from '../models/ITransaction';
 
-export const useCheckDate = (transactions: ITransaction[]) => {
+export const useCheckDate = async (transactions: ITransaction[]) => {
   const [user] = useAtom(userAtom);
 
-  const checkAndCreateTransaction = () => {
-    transactions.forEach((transaction) => {
-      if (
-        transaction.reccurancy === 'recurring' &&
-        isSameDate(new Date(transaction.date), CURRENT_DATE)
-      ) {
-        createTransactionDocument(
-          user,
-          transaction,
-          new Date(transaction.date)
-        );
-      }
-    });
-  };
+  for (const transaction of transactions) {
+    if (
+      transaction.reccurancy === 'recurring' &&
+      isSameDate(new Date(transaction.date), CURRENT_DATE)
+    ) {
+      await createTransactionDocument(
+        user,
+        transaction,
+        new Date(transaction.date)
+      );
+    }
+  }
+};
 
-  const isSameDate = (transactionDate: Date, currentDate: Date) => {
-    return (
-      transactionDate.getDate() === currentDate.getDate() &&
-      transactionDate.getMonth() === currentDate.getMonth() &&
-      transactionDate.getFullYear() === currentDate.getFullYear()
-    );
-  };
-
-  checkAndCreateTransaction();
+const isSameDate = (transactionDate: Date, currentDate: Date) => {
+  return (
+    transactionDate.getDate() === currentDate.getDate() &&
+    transactionDate.getMonth() === currentDate.getMonth() &&
+    transactionDate.getFullYear() === currentDate.getFullYear()
+  );
 };
