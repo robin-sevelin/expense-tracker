4316ec2607b52127881f4e8258f14b466a5a52d5
robin-sevelin <robinsevelin@hotmail.com>
2024-01-23 13:37:50 +0100

    header nav

--- app/components/header/Navigation.tsx
+++ app/components/header/Navigation.tsx
@@ -5,16 +5,39 @@ import { ROUTES } from '../../constants/constants';
 
 const Navigation = () => {
   return (
-    <div className='sidebar'>
-      <nav>
-        <ul>
+    <div className='drawer drawer-end'>
+      <input id='my-drawer-4' type='checkbox' className='drawer-toggle' />
+      <div className='drawer-content'>
+        <label
+          aria-label='close sidebar button'
+          htmlFor='my-drawer-4'
+          className='drawer-button btn btn-circle'
+        >
+          <svg
+            className='swap-off fill-current'
+            xmlns='http://www.w3.org/2000/svg'
+            width='32'
+            height='32'
+            viewBox='0 0 512 512'
+          >
+            <path d='M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z' />
+          </svg>
+        </label>
+      </div>
+      <div className='drawer-side'>
+        <label
+          htmlFor='my-drawer-4'
+          aria-label='close sidebar'
+          className='drawer-overlay'
+        ></label>
+        <ul className='menu p-4 w-80 min-h-full bg-base-200 text-base-content'>
           {ROUTES.map((route) => (
             <li key={route.id}>
               <Link href={route.url}>{route.text.toUpperCase()}</Link>
             </li>
           ))}
         </ul>
-      </nav>
+      </div>
     </div>
   );
 };
