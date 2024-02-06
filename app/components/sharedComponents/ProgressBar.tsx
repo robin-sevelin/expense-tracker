40b5a8befb9e9f6aad761850f8367985c672fc0c
robin-sevelin <robinsevelin@hotmail.com>
2024-02-06 12:35:34 +0100

    dont render progress if zero balance

--- app/components/sharedComponents/ProgressBar.tsx
+++ app/components/sharedComponents/ProgressBar.tsx
@@ -29,7 +29,7 @@ const ProgressBar = () => {
 
   return (
     <>
-      {balance && (
+      {balance === 0 && (
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className=' p-2 m-1'>
             <h3 className='font-bold'>
